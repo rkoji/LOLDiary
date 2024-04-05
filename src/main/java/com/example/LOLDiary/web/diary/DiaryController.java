344e7a08534a24f8622f4ef4fd4e0ce8a9976841
jiyeon <wl0wn1212@gmail.com>
2024-04-05 23:04:13 +0900

    [Refactor] api를 불러오는 로직 별도의 클래스로 분리

--- src/main/java/com/example/LOLDiary/web/diary/DiaryController.java
+++ src/main/java/com/example/LOLDiary/web/diary/DiaryController.java
@@ -1,6 +1,5 @@
 package com.example.LOLDiary.web.diary;
 
-import com.example.LOLDiary.domain.diary.Diary;
 import com.example.LOLDiary.domain.member.Member;
 import com.example.LOLDiary.domain.summoner.Summoner;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,7 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import static com.example.LOLDiary.web.session.SessionConst.LOGIN_MEMBER;
 
@@ -22,7 +24,7 @@ public class DiaryController {
     private final DiaryService diaryService;
 
     @GetMapping("/create")
-    public String createFrom(@Validated @ModelAttribute("diary") DiaryDto dto,HttpServletRequest request, Model model) {
+    public String createFrom(@Validated @ModelAttribute("diary") DiaryDto dto, HttpServletRequest request, Model model) {
         Member loginMember = getMemberFromSession(request);
         String nickname = loginMember.getNickname();
         model.addAttribute("nickname", nickname);
@@ -30,7 +32,7 @@ public class DiaryController {
     }
 
     @PostMapping("/create")
-    public String createDiary(@Validated @ModelAttribute("diary") DiaryDto dto, BindingResult bindingResult, HttpServletRequest request){
+    public String createDiary(@Validated @ModelAttribute("diary") DiaryDto dto, BindingResult bindingResult, HttpServletRequest request) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
@@ -38,12 +40,12 @@ public class DiaryController {
         Member loginMember = getMemberFromSession(request);
         String nickname = loginMember.getNickname();
         String diaryText = dto.getDiaryText();
-        diaryService.createDiary(nickname,diaryText);
+        diaryService.createDiary(nickname, diaryText);
         return "redirect:/";
     }
 
     @GetMapping("/summoner")
-    public String getSummoner(HttpServletRequest request , Model model) {
+    public String getSummoner(HttpServletRequest request, Model model) {
         Member loginMember = getMemberFromSession(request);
         String nickname = loginMember.getNickname();
         Summoner summonerData = diaryService.getSummonerData(nickname);
@@ -53,6 +55,6 @@ public class DiaryController {
 
     private Member getMemberFromSession(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
-        return  (Member) session.getAttribute(LOGIN_MEMBER);
+        return (Member) session.getAttribute(LOGIN_MEMBER);
     }
 }
