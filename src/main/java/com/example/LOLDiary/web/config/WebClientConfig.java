412cbc9e45683e7c01c97809395c18b985198bfa
jiyeon <wl0wn1212@gmail.com>
2024-06-20 11:24:01 +0900

    [Refactor] WebClient timeout 추가

--- src/main/java/com/example/LOLDiary/web/config/WebClientConfig.java
+++ src/main/java/com/example/LOLDiary/web/config/WebClientConfig.java
@@ -1,19 +1,33 @@
 package com.example.LOLDiary.web.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
 
     @Value("${riot.api}")
     private String apiKey;
+    private final Duration timeoutDuration = Duration.ofSeconds(30);
 
     @Bean
     public WebClient webClient(){
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, (int) timeoutDuration.toMillis())
+                .doOnConnected(conn -> conn.addHandlerFirst(new ReadTimeoutHandler(Math.toIntExact(timeoutDuration.getSeconds()))));
+
+        ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
         return WebClient.builder()
+                .clientConnector(connector)
                 .defaultHeader("X-Riot-Token", apiKey)
                 .build();
     }
