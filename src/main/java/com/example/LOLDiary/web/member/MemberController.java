fbd9f0a570d9db0dd6b2a5b6c43426fa815f1f87
jiyeon <wl0wn1212@gmail.com>
2024-07-02 15:57:02 +0900

    [Feat] 회원가입 Swagger API 작성

--- src/main/java/com/example/LOLDiary/web/member/MemberController.java
+++ src/main/java/com/example/LOLDiary/web/member/MemberController.java
@@ -2,6 +2,8 @@ package com.example.LOLDiary.web.member;
 
 import com.example.LOLDiary.domain.member.Member;
 import com.example.LOLDiary.web.member.dto.JoinRequestDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Tag(name = "회원가입 컨트롤러", description = "회원가입 API 입니다.")
 @Slf4j
 @Controller
 @RequiredArgsConstructor
@@ -20,11 +23,13 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @Operation(summary = "회원가입 화면", description = "사용자가 회원가입 정보를 입력합니다.")
     @GetMapping("/add")
     public String joinForm(@ModelAttribute Member member) {
         return "members/joinForm";
     }
 
+    @Operation(summary = "회원가입",description = "사용자가 회원가입을 요청합니다.")
     @PostMapping("/add")
     public String save(@Validated @ModelAttribute("member") JoinRequestDto dto, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
--- src/main/java/com/example/LOLDiary/web/member/dto/JoinRequestDto.java
+++ src/main/java/com/example/LOLDiary/web/member/dto/JoinRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.LOLDiary.web.member.dto;
 
 import com.example.LOLDiary.domain.member.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
@@ -10,15 +11,19 @@ import lombok.*;
 public class JoinRequestDto {
 
     @NotEmpty
+    @Schema(description = "사용자 ID",example = "login123")
     private String loginId;
 
     @NotEmpty
+    @Schema(description = "사용자 닉네임",example = "nickname")
     private String nickname;
 
     @NotEmpty
+    @Schema(description = "사용자 비밀번호",example = "1234!")
     private String password;
 
     @NotEmpty
+    @Schema(description = "사용자 태그",example = "KR1")
     private String tag;
 
     public Member toEntity(){
