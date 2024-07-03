c6b2bfa7585e00005c1e5d082d0a6c228743e234
jiyeon <wl0wn1212@gmail.com>
2024-07-03 17:54:15 +0900

    [Feat] 회원 가입 시 중복 사용자 예외 처리

--- src/main/java/com/example/LOLDiary/web/member/impl/MemberServiceImpl.java
+++ src/main/java/com/example/LOLDiary/web/member/impl/MemberServiceImpl.java
@@ -2,12 +2,15 @@ package com.example.LOLDiary.web.member.impl;
 
 import com.example.LOLDiary.domain.member.Member;
 import com.example.LOLDiary.domain.member.MemberRepository;
-import com.example.LOLDiary.web.member.dto.JoinRequestDto;
+import com.example.LOLDiary.exception.CustomException;
 import com.example.LOLDiary.web.member.MemberService;
+import com.example.LOLDiary.web.member.dto.JoinRequestDto;
 import com.example.LOLDiary.web.member.dto.JoinResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import static com.example.LOLDiary.exception.ErrorCode.USER_ALREADY_EXISTS;
 
 @Slf4j
 @Service
@@ -18,7 +21,13 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public JoinResponseDto saveMember(JoinRequestDto dto) {
+        memberRepository.findByLoginId(dto.getLoginId())
+                .ifPresent(member -> {
+                    throw new CustomException(USER_ALREADY_EXISTS);
+                });
+
         Member joinMember = memberRepository.save(dto.toEntity());
+
         return JoinResponseDto.saveJoinResponseDto(joinMember);
     }
 }
